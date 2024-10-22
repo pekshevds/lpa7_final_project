45d2dfc1d3da96d0d851574d377cd2199b967556
pekshevds <pekshev.ds@gmail.com>
2024-10-22 20:47:27 +0300

    add custome users model

--- server/settings.py
+++ server/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "auth_app.apps.AuthAppConfig",
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,4 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "auth_app.LPA7User"
